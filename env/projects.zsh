2fe0c7bc544861e4debfd9f1679f46f7817a372e
Michael Haynes <antihaynes@gmail.com>
2022-05-07 18:40:45 -0500

    Add projects.zsh

---
+++ env/projects.zsh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Config for zsh project plugin(s)
+
+export PROJECTS=~/Source/projects
+export PROJECTS_RECIPES=${PROJECTS}/.recipes
+export PROJECTS_TMP=${PROJECTS}/tmp
+export ZPWR_LEARN_COMMAND='mysql'
+export ZPWR_SCHEMA_NAME="root"
+export ZPWR_TABLE_NAME="Learning"
+
